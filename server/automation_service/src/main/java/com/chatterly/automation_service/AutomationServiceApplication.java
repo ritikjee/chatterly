04ac2b5f41f0d209ca85bce89fcbede0f5377dda
ritikjee <ritikj.ug21.ee@nitp.ac.in>
2025-01-08 17:42:14 +0530

    added services for subscription, automation and integration

---
+++ server/automation_service/src/main/java/com/chatterly/automation_service/AutomationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chatterly.automation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationServiceApplication.class, args);
+	}
+
+}
